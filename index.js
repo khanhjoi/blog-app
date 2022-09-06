3601a9b22049e51ea833748600e603111dbdf177
khanhjoi <khanhjoi1@gmail.com>
2022-09-06 09:31:21 +0700

    upload file to database

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const BlogPost = require("./model/BlogPost");
+const fileUpload = require("express-fileupload");
 
 const app = new express();
 const Schema = mongoose.Schema;
@@ -12,6 +13,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
 
 // connect to database and default
 mongoose.connect('mongodb://localhost:27017/my_database', {useNewUrlParser: true});
@@ -38,23 +40,27 @@ app.get('/contact', (req, res) => {
     res.render('contact');
 });
 
-app.get('/post/:id', async (req, res) => {
-    const blogpost = await BlogPost.findById(req.params.id);
-    // console.log(req.params.id)
-    res.render('post',  {
-        blogpost
-    });
-});
-
 app.get('/post/new', (req, res) => {
     res.render('create');
 });
 app.post('/post/store', async (req, res) => {
-    await BlogPost.create(req.body)
-    res.redirect('/');
+    let image = req.files.image;
+    image.mv(path.resolve(__dirname, '/public/img', image.name), async (err) => {
+        await BlogPost.create({
+            ...req.body,
+            image: '/img/' + image.name,
+            }
+        )
+        res.redirect('/');
+    })
 });
 
-
+app.get('/post/:id', async (req, res) => {
+    const blogpost = await BlogPost.findById(req.params.id);
+    res.render('post',  {
+        blogpost
+    });
+});
 
 
 
--- model/BlogPost.js
+++ model/BlogPost.js
@@ -4,7 +4,13 @@ const Schema = mongoose.Schema;
 mongoose.connect('mongodb://localhost:27017/my_database', {useNewUrlParser: true});
 const BlogPostSchema = new Schema({
     title: String,
-    body: String
+    body: String,
+    username: String,
+    datePosted: {
+        type: Date,
+        default: new Date(),
+    },
+    image: String
 })
 
 const BlogPost = mongoose.model('BlogPost',BlogPostSchema);
